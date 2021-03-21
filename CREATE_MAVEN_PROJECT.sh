ed23f70defe6f82767125d534ffc0bfc1df6f87c
dalayach <davidalayachew@gmail.com>
2021-03-21 00:07:21 -0400

    Fixed the create script, and updated the .gitignore

--- CREATE_MAVEN_PROJECT.sh
+++ CREATE_MAVEN_PROJECT.sh
@@ -1,39 +1,79 @@
-read -p "Enter the name of your new Maven project: " artifactId
+create_maven_project() {
 
-#mvn archetype:generate -DgroupId=io.github.dalayach -DartifactId=$artifactId -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
+	read -p "Enter the name of your new Maven project: " artifact_id
 
-# (1) prompt user, and read command line argument
-read -p "Please go to GitHub now and make your repository! Press Y when you have done so! - " answer
+	mvn archetype:generate -DgroupId=io.github.dalayach -DartifactId=$artifact_id -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
 
-while true
-do
+}
 
-  # (2) handle the input we were given
-  case $answer in
-   [yY]* ) break;;
+make_sure_user_has_created_repository_on_git_hub() {
 
-   * )     read -p "You must press Y in order to continue - " answer;;
-  esac
-done
 
-echo ""
-echo ""
+	read -p "Please go to GitHub now and make your repository! Press Y when you have done so! - " answer
 
-arrDomains=()
-echo "Enter the domain names :"
+	while true
+	do
 
-while read domain
-do
-    arrDomains+=($domain)
-    # do processing with each domain
-done
+  		case $answer in
+   			[yY]* ) break;;
 
-echo "Domain List : ${arrDomains[@]}"
+			* )     read -p "You must press Y in order to continue - " answer;;
+		esac
 
-echo ""
-echo ""
-echo "Please enter the option that says \"...or create a new repository on the command line\"!"
-echo "Be sure to press new line a few times to make sure it works."
-echo ""
+	done
 
+	echo ""
+	echo ""
+
+
+
+}
+
+run_git_repo_initialization_script() {
+
+	IFS=$'\n'
+	counter=0;
+
+	declare -a final
+
+	echo "Please enter the option that says \"...or create a new repository on the command line\"!"
+	echo "Be sure to press new line a few times to make sure it works."
+	read final[$counter]
+
+	output=${final[$counter]}
+
+	while [[ ! -z "$output" ]]
+	do
+
+	        ((counter=counter+1))
+	        read final[$counter]
+	        output=${final[$counter]}
+
+	done
+
+}
+
+run_git_command() {
+
+        cd "$artifact_id"
+
+        for each in ${final[@]}
+        do
+
+                echo "$each"
+
+        done
+
+	cd ..
+	pwd
+
+}
+
+create_maven_project
+
+make_sure_user_has_created_repository_on_git_hub
+
+run_git_repo_initialization_script
+
+run_git_command
 
